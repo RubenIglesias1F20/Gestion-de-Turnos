8f4eb54a2d9ebffb3826710f03732e620d942083
IA23005 <rubenedgardo70080503@gmail.com>
2025-09-25 17:30:23 -0600

    Structure Project

---
+++ src/test/java/com/tailwait/gestionturnos/GestionturnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailwait.gestionturnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionturnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
